74a1740d7f963a165886d67e901d7c77153e85ea
Berlimor <eldaryusufovooo@gmail.com>
2023-06-06 18:43:41 +0300

    Minor commit

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
-from db.models import User
+from db.models import User 
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = "__all__"
--- db/migrations/0001_initial.py
+++ db/migrations/0001_initial.py
@@ -4,21 +4,27 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='User',
+            name="User",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('first_name', models.CharField(max_length=20)),
-                ('last_name', models.CharField(max_length=20)),
-                ('username', models.CharField(max_length=20)),
-                ('pwd', models.CharField(max_length=50)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("first_name", models.CharField(max_length=20)),
+                ("last_name", models.CharField(max_length=20)),
+                ("username", models.CharField(max_length=20)),
+                ("pwd", models.CharField(max_length=50)),
             ],
         ),
     ]
